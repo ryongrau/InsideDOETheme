83f7662311d41c7e9118dd5899fe42426360557a
ryongrau <spankleberry@gmail.com>
2015-05-19 10:46:03 -0400

    FRESH REINSTALL
    A fresh start from the AT Subtheme as provided by Dan Gibson.  Favicon
    and Screenshot and insidedoetheme.info are the only changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# InsideDOETheme
-Theme for the inside DOE blog
-To be installed under AdaptiveTheme
--- css/set-background.css.php
+++
@@ -1,8 +0,0 @@
-#page-wrapper {
-	background-image:url("<?php
-			echo "backgrounds/16821059326_b9115c3004_k.jpg";
-		?>");
-	background-size:cover;
-	background-repeat:no-repeat;
-	background-attachment:fixed;
-}
